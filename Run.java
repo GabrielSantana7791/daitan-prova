f20a8e89617cb595dca49e16e0bde00cd35085b7
Gabriel de Paula Santana <Gabrielsantana7791@gmail.com>
2023-02-13 10:13:30 -0300

    recode; add replace empty space with zero

--- Run.java
+++ Run.java
@@ -2,67 +2,63 @@ package a;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class Run {
-	
-	   /*
-    quarters = 25;
-	 dimes = 10;				
-	 nickels = 5;
-	 pennies = 1;
-*/
-static int[] coins = {25, 10, 5, 1};
 
-public static void main(String[] args) {
-	
-	int numberToTest = 5;
-    Set<List<Integer>> result = makeChange(numberToTest);
-    
-    //print every single element inside result
-    for (List<Integer> change : result) {
-        System.out.println(change);
-    }
-}
+	static int[] coins = {1, 5, 10, 25};
+	static ArrayList<Integer>possibilities = new ArrayList<>();
 
-    public static Set<List<Integer>> makeChange(int numberToTest) {
-    	
-        Set<List<Integer>> result = new HashSet<>();
-     
-        
-        List<Integer> currentChange = new ArrayList<>();
-        makeChangeEveryCoin(numberToTest, 0, currentChange, result);
-        
-        return result;
-    }
+	public static void main(String[] args) {
+		HashSet<ArrayList<Integer>>result = makeChange(21);
 
-    private static void makeChangeEveryCoin(int numberToTest, int coinIndex, List<Integer> currentChange, Set<List<Integer>> result) {
-    	
-    	//if the number is equal 0, there is no point testing
-        if (numberToTest == 0) {
-            result.add(new ArrayList<>(currentChange));
-            
-            return;
-        }
-        
-    	//stop the loop
-        if (coinIndex >= coins.length) {
-        
-            return;
-        }
-        
-        //all number posibilites. Ex: if the max or numberToTest = 25, I can just add once
-        int maxCoins = numberToTest / coins[coinIndex];
-        
-        //as I said, there is no point testing an infinite range of numbers
-        for (int i = 0; i <= maxCoins; i++) {
-        	
-            currentChange.add(i);
-            makeChangeEveryCoin(numberToTest - i * coins[coinIndex], coinIndex + 1, currentChange, result);
-            currentChange.remove(currentChange.size() - 1);
-        }
-    }
+		for(ArrayList<Integer> element : result) {
+			System.out.println(element.toString());
+		}
+	}
 
-   
+	static HashSet<ArrayList<Integer>> makeChange(int numberToTest, int coin, int sum, HashSet<ArrayList<Integer>>result, ArrayList<Integer> currentChange) {
+		if(coin > coins.length -1) {
+			return null;
+		}
+
+		for(int i=0; i<=possibilities.get(coin); i++) {
+			if(sum == numberToTest) {
+				ArrayList<Integer> seq2 = (ArrayList<Integer>) currentChange.clone();
+
+				//replace empty spaces with zero
+				for(int b=4-seq2.size(); b>0; b--) {
+					seq2.add(0);
+				}
+				result.add(seq2);
+
+				return null;
+			}
+
+			sum += coins[coin] * i;
+			currentChange.add(i);
+			makeChange(numberToTest, coin + 1, sum, result, currentChange);
+			sum -= coins[coin] * i;
+			currentChange.remove(currentChange.size() -1);
+		}
+
+		return result;
+	}
+
+	static HashSet<ArrayList<Integer>> makeChange(int n){
+		for(int i : coins) {
+			int r = n / i;
+
+			if(r < 0) {
+				possibilities.add(0);
+			}else {
+				possibilities.add(r);
+			}
+		}
+
+		ArrayList<Integer> seq = new ArrayList<>();
+		HashSet<ArrayList<Integer>>result = new HashSet<>();
+		makeChange(n,0, 0, result, seq);
+
+		return result;		
+	}
 }
